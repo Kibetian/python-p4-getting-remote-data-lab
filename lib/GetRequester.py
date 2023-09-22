a7dc518fd85b0652638c1b0fd9c4753f84ff5929
Kibetian <126007714+Kibetian@users.noreply.github.com>
2023-09-22 13:03:18 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,23 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
+            return response.content  # Return response content as bytes
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+            return None
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        if response_body:
+            try:
+                return json.loads(response_body)
+            except json.JSONDecodeError as e:
+                print(f"Failed to parse JSON: {e}")
+                return None
